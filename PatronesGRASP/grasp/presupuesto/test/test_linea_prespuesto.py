72b0f65101523d5aa3f463960604cf2039899a03
Victor Valotto <vvalotto@gmail.com>
2021-03-01 17:16:12 -0300

    GRASP - Polimorfismo

--- PatronesGRASP/grasp/presupuesto/test/test_linea_prespuesto.py
+++ PatronesGRASP/grasp/presupuesto/test/test_linea_prespuesto.py
@@ -18,3 +18,21 @@ class TestLineaPresupuesto(TestCase):
         self._linea_presupuesto.presupuestar_mes('Enero', 100)
         self._linea_presupuesto.presupuestar_mes('Febrero', 200)
         self.assertEqual(self._linea_presupuesto.obtener_presupuesto_linea(), 300)
+
+
+class TestLineaPresupuestolOtraMoneda(TestCase):
+
+    def setUp(self) -> None:
+        self._linea_presupuesto = LineaPresupuestoOtrasMonedas("Licencias Sistema 2", 'Dolar')
+        self._linea_presupuesto.presupuestar_mes('Enero', 50, 90)
+        print(self._linea_presupuesto.denominacion)
+
+    def test_obtener_presupuesto_mes(self):
+        self._linea_presupuesto.presupuestar_mes('Enero', 100, 95)
+        gasto = self._linea_presupuesto.obtener_presupuesto_mes('Enero')
+        self.assertEqual(gasto, 9500)
+
+    def test_obtener_presupuesto_linea(self):
+        self._linea_presupuesto.presupuestar_mes('Enero', 100, 90)
+        self._linea_presupuesto.presupuestar_mes('Febrero', 200, 95)
+        self.assertEqual(self._linea_presupuesto.obtener_presupuesto_linea(), 28000)
