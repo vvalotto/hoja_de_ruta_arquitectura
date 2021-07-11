2d29db288b6a58539a89b07f4a3431cccf4081cf
Victor Valotto <vvalotto@gmail.com>
2021-07-11 13:03:36 -0300

    Modificaciones en Persona y Paciente

--- PatronesGRASP/LoD/test/ejemplo_turno.py
+++ PatronesGRASP/LoD/test/ejemplo_turno.py
@@ -2,8 +2,8 @@
 Instancia de un turno para uso en caso de prueba
 """
 
-from test.ejemplo_paciente import fabricar_paciente, fabricar_paciente_2, fabricar_paciente_3
-from LoD.entidades.turno import *
+from ejemplo_paciente import fabricar_paciente, fabricar_paciente_2, fabricar_paciente_3
+from PatronesGRASP.LoD.LoD.entidades.turno import *
 from datetime import date, time
 
 
@@ -21,6 +21,7 @@ def fabricar_turno_2()-> Turno:
     turno.dia = date(2021, 2, 4)
     turno.hora = time(16, 0, 0, 0)
     return turno
+
 
 def fabricar_turno_3()-> Turno:
     turno = Turno()
--- PatronesGRASP/LoD/test/prueba_persona.py
+++ PatronesGRASP/LoD/test/prueba_persona.py
@@ -6,5 +6,5 @@ mi_documento = Identificacion('DNI', 14367839)
 
 una_persona = Persona(mi_nombre, mi_documento, date(2009, 4, 23), 'M')
 
-print(una_persona)
+print(una_persona.nombre)
 print(una_persona.fecha_de_nacimiento)
