6f54a49af7a1e7311a41c2eacd455986f0e4add9
Victor Valotto <vvalotto@gmail.com>
2021-07-24 15:52:55 -0300

    GRASP - Variaciones Protegidas Parte 1

--- PatronesGRASP/presupuesto/servicios/notificador.py
+++ PatronesGRASP/presupuesto/servicios/notificador.py
@@ -1,5 +1,29 @@
+"""
+Simula las Variaciones Protegidas y Polimorfismo de un Notificador
+"""
 
-class Notificador:
+from abc import ABC, abstractmethod
+
+
+class Notificador(ABC):
+
+    @abstractmethod
+    def notificar(self, mensaje):
+        pass
+
+
+class NotificadorConsola(Notificador):
 
     def notificar(self, mensaje):
         print(mensaje)
+
+
+class NotificadorArchivo(Notificador):
+
+    def notificar(self, mensaje):
+        try:
+            notificacion = open('notificacion_presupuesto.txt', 'w')
+            notificacion.write(mensaje)
+            notificacion.close()
+        except IOError:
+            print('Error al grabar la linea')
